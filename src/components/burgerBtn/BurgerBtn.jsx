20f460afd56a72aa685d6d07bf3bd7b7bdf28461
Stepan Dordiai <stepandordiai@gmail.com>
2025-01-03 16:05:58 +0200

    v1.4.2 Fix bugs

--- src/components/burgerBtn/BurgerBtn.jsx
+++ src/components/burgerBtn/BurgerBtn.jsx
@@ -1,18 +1,21 @@
 import "./BurgerBtn.css";
+import "./../header/Header.css";
 
 function BurgerBtn() {
-  function toggleBtn() {
-    document
-      .querySelector(".burger-button")
-      .classList.toggle("active-burger-button");
-    document.querySelector(".header").classList.toggle("active-header");
-    document.querySelector(".header-nav").classList.toggle("active-header-nav");
-    document
-      .querySelector(".header-socials")
-      .classList.toggle("active-header-socials");
-  }
+    function toggleBtn() {
+        document
+            .querySelector(".burger-button")
+            .classList.toggle("active-burger-button");
+        document.querySelector(".header").classList.toggle("active-header");
+        document
+            .querySelector(".header-nav")
+            .classList.toggle("active-header-nav");
+        document
+            .querySelector(".header-socials")
+            .classList.toggle("active-header-socials");
+    }
 
-  return <div onClick={toggleBtn} className="burger-button"></div>;
+    return <div onClick={toggleBtn} className="burger-button"></div>;
 }
 
 export default BurgerBtn;
